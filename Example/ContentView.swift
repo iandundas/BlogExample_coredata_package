d7905bdf6ec2c805c537bee6f5ba4a226ee7f026
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2023-02-02 15:12:46 +0100

    Last changes

--- Example/ContentView.swift
+++ Example/ContentView.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 import CoreData
+import Persistence
+
+extension Item: Identifiable {}
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
@@ -87,4 +90,24 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
+}
+
+extension PersistenceController {
+	static var preview: PersistenceController = {
+		let result = PersistenceController(inMemory: true)
+		let viewContext = result.container.viewContext
+		for _ in 0..<10 {
+			let newItem = Item(context: viewContext)
+			newItem.timestamp = Date()
+		}
+		do {
+			try viewContext.save()
+		} catch {
+			// Replace this implementation with code to handle the error appropriately.
+			// fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+			let nsError = error as NSError
+			fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+		}
+		return result
+	}()
 }
--- Example/ExampleApp.swift
+++ Example/ExampleApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Persistence
 
 @main
 struct ExampleApp: App {
